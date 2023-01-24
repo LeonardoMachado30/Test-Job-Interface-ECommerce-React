6771fe3baf5005095a4c398f3f03fa38d3fd10ea
Flavio Leonardo Machado <60984765+LeonardoMachado30@users.noreply.github.com>
2023-01-24 14:56:10 -0300

    CHANGE: index and _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@/styles/Global.component.css'
+import '@/styles/Global.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { CardList, Header } from '../components/index';
+import Head from "next/head";
+import Image from "next/image";
+import { CardList, Header, Footer, ShoppingCart } from "../components/index";
 
 export default function Home() {
   return (
@@ -11,10 +11,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-      <Header />
       <main>
+        <Header />
         <CardList />
+        <Footer />
+        <ShoppingCart />
       </main>
     </>
-  )
+  );
 }
